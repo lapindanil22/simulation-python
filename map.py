10d79f537eaceb10531d339f382678a3dea60b97
Danil Lapin <lapindanil22@gmail.com>
2023-10-23 23:15:13 +0700

    project structure updated

--- map.py
+++ map.py
@@ -1,17 +1,17 @@
 class Map:
     def __init__(self, size) -> None:
         self.size = size
-        self.map = {}
+        self.cells = {}
 
     def clear_cell(self, coords):
-        self.map.pop(coords, None)
+        self.cells.pop(coords, None)
 
     def set_entity(self, coords, entity):  # TODO boundary check
-        self.map[coords] = entity
+        self.cells[coords] = entity
 
     def move_entity(self, coords_from, coords_to):
-        self.set_entity(coords_to, self.map[coords_from])
+        self.set_entity(coords_to, self.cells[coords_from])
         self.clear_cell(coords_from)
 
     def as_dict(self):
-        return self.map
+        return self.cells
