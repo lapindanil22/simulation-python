86cff49a42010ef06a7c9390254c9c0994db7ebc
Danil Lapin <lapindanil22@gmail.com>
2023-11-12 00:54:29 +0700

    Refactoring

---
+++ renderer.py
@@ -0,0 +1,19 @@
+import pygame
+
+
+class Renderer:
+    def __init__(self, map_size, cell_size_px):
+        self.__cell_size_px = cell_size_px
+        self.__screen = pygame.display.set_mode((map_size[0] * cell_size_px,
+                                                 map_size[1] * cell_size_px))
+
+    def render(self, cells):
+        pygame.draw.rect(self.__screen, "black",
+                         (0, 0, cells.size[0] * self.__cell_size_px,
+                          cells.size[1] * self.__cell_size_px))
+        for coord, entity in cells.to_dict():
+            pygame.draw.rect(self.__screen, entity.color, (coord[0] * self.__cell_size_px,
+                                                           coord[1] * self.__cell_size_px,
+                                                           self.__cell_size_px,
+                                                           self.__cell_size_px))
+        pygame.display.update()
